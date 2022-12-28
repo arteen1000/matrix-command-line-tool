b9d0f6ad006926aaee32fc156d43c068164dceb9
Arteen Abrishami <arteen1000@gmail.com>
2022-12-27 18:59:57 -0800

    Initial Commit

---
+++ matrix-mult/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  matrix-mult
+//
+//  Created by Arteen Abrishami on 12/27/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
